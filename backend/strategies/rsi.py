804ef4d7a0530a29cb02a7f38fa9a4fc2d6db47e
Darshbir Singh <darshbir004@gmail.com>
2026-06-10 19:23:14 +0800

    refactor: parameters

--- backend/strategies/rsi.py
+++ backend/strategies/rsi.py
@@ -1,16 +1,16 @@
 import numpy as np
 from backend.data import fetch
-def rsi(df , days=14):
+def rsi(df , period = 14 , oversold=30, overbought=70):
     df['Change'] = df['Close'].diff()
     df['Gain'] = np.where(df['Change'] > 0,  df['Change'], 0)
     df['Loss'] = np.where(df['Change'] < 0,  df['Change'].abs() , 0)
-    df['Avg_Gain'] = df['Gain'].rolling(window= days).mean()
-    df['Avg_Loss'] = df['Loss'].rolling(window= days).mean()
+    df['Avg_Gain'] = df['Gain'].rolling(window= period).mean()
+    df['Avg_Loss'] = df['Loss'].rolling(window= period).mean()
     df["RS"] = df["Avg_Gain"] / df["Avg_Loss"]
     df['RSI'] = 100 - (100/ (1 + df['RS']))
     df['RSI_Signal'] = np.where(
-        df['RSI'] > 70, -1, 
-        np.where(df['RSI'] <30 , 1 , 0)) 
+        df['RSI'] > overbought, -1, 
+        np.where(df['RSI'] < oversold , 1 , 0)) 
     
     return df
     
